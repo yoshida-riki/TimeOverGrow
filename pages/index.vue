3c99ff9f2d5564157a5086087afc0fa724ffb3bd
yoshida riki <ab.jpnet.01@gmail.com>
2020-12-18 21:42:11 +0900

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,32 @@
 <template>
-<div id="animated-number-demo">
-  <input v-model.number="number" type="number" step="20" />
-  <p>{{ animatedNumber }}</p>
-</div>
+  <div>
+    <input
+      v-model.number="num" type="number"
+    >
+    <AnimatedNumber
+      :value="num"
+    >
+    </AnimatedNumber>
+  </div>
 </template>
 
-<script> 
+<script>
+import AnimatedNumber from '../components/AnimatedNumber';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
 export default {
-  data() {
+  components: { AnimatedNumber },
+  data () {
     return {
-      number: 0,
-      tweenedNumber: 0
-    }
-  },
-  computed: {
-    animatedNumber() {
-      return this.tweenedNumber.toFixed(0)
-    }
-  },
-  watch: {
-    number(newValue) {
-      gsap.to(this.$data, { duration: 0.5, tweenedNumber: newValue })
-    }
+      num: 0,
+    };
   }
-}
-Vue.createApp(Demo).mount('#animated-number-demo')
+};
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 20vh;
-  margin-top: 100px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-  font-size: 50px;
-}
+
 </style>
